aa235fb393697c2ab46cdc6eabe8a8ed6373e083
Matheus Martins da Rocha <124638744+MatheusMartinsR@users.noreply.github.com>
2024-06-05 19:08:09 -0300

    fix: package

--- packages.sql
+++ packages.sql
@@ -1,43 +1,18 @@
--- PACKAGES
-
-
--- Criação do package specification
+-- Criação da package
 CREATE OR REPLACE PACKAGE AUDIT_PKG AS
     PROCEDURE AUDIT_TB_USUARIO;
     PROCEDURE AUDIT_TB_INTERACOES;
 END AUDIT_PKG;
 
 
--- Criação do package body
+-- Corpo da package
 CREATE OR REPLACE PACKAGE BODY AUDIT_PKG AS
-
     PROCEDURE AUDIT_TB_USUARIO IS
         v_operacao VARCHAR2(10);
         v_dados_antes CLOB;
         v_dados_depois CLOB;
     BEGIN
-        IF INSERTING THEN
-            v_operacao := 'INSERT';
-            v_dados_depois := 'ID_USUARIO=' || :NEW.ID_USUARIO || ', ID_PERFIL=' || :NEW.ID_PERFIL || 
-                              ', ID_REGIAO=' || :NEW.ID_REGIAO || ', EMAIL_USUARIO=' || :NEW.EMAIL_USUARIO || 
-                              ', CNPJ_USUARIO=' || :NEW.CNPJ_USUARIO || ', CPF_USUARIO=' || :NEW.CPF_USUARIO;
-        ELSIF UPDATING THEN
-            v_operacao := 'UPDATE';
-            v_dados_antes := 'ID_USUARIO=' || :OLD.ID_USUARIO || ', ID_PERFIL=' || :OLD.ID_PERFIL || 
-                             ', ID_REGIAO=' || :OLD.ID_REGIAO || ', EMAIL_USUARIO=' || :OLD.EMAIL_USUARIO || 
-                             ', CNPJ_USUARIO=' || :OLD.CNPJ_USUARIO || ', CPF_USUARIO=' || :OLD.CPF_USUARIO;
-            v_dados_depois := 'ID_USUARIO=' || :NEW.ID_USUARIO || ', ID_PERFIL=' || :NEW.ID_PERFIL || 
-                              ', ID_REGIAO=' || :NEW.ID_REGIAO || ', EMAIL_USUARIO=' || :NEW.EMAIL_USUARIO || 
-                              ', CNPJ_USUARIO=' || :NEW.CNPJ_USUARIO || ', CPF_USUARIO=' || :NEW.CPF_USUARIO;
-        ELSIF DELETING THEN
-            v_operacao := 'DELETE';
-            v_dados_antes := 'ID_USUARIO=' || :OLD.ID_USUARIO || ', ID_PERFIL=' || :OLD.ID_PERFIL || 
-                             ', ID_REGIAO=' || :OLD.ID_REGIAO || ', EMAIL_USUARIO=' || :OLD.EMAIL_USUARIO || 
-                             ', CNPJ_USUARIO=' || :OLD.CNPJ_USUARIO || ', CPF_USUARIO=' || :OLD.CPF_USUARIO;
-        END IF;
-     
-        INSERT INTO TB_AUDITORIA (ID_AUDITORIA, TABELA, OPERACAO, USUARIO, DATA_HORA, DADOS_ANTES, DADOS_DEPOIS)
-        VALUES (SEQ_AUDITORIA.NEXTVAL, 'TB_USUARIO', v_operacao, USER, SYSTIMESTAMP, v_dados_antes, v_dados_depois);
+        NULL; 
     END AUDIT_TB_USUARIO;
 
     PROCEDURE AUDIT_TB_INTERACOES IS
@@ -45,28 +20,7 @@ CREATE OR REPLACE PACKAGE BODY AUDIT_PKG AS
         v_dados_antes CLOB;
         v_dados_depois CLOB;
     BEGIN
-        IF INSERTING THEN
-            v_operacao := 'INSERT';
-            v_dados_depois := 'ID_INTERACOES=' || :NEW.ID_INTERACOES || ', ID_USUARIO=' || :NEW.ID_USUARIO || 
-                              ', ID_EVENTO=' || :NEW.ID_EVENTO || ', ID_TP_INTERACAO=' || :NEW.ID_TP_INTERACO || 
-                              ', ID_INCIDENTES=' || :NEW.ID_INCIDENTES || ', INTERACOES=' || :NEW.INTERACOES;
-        ELSIF UPDATING THEN
-            v_operacao := 'UPDATE';
-            v_dados_antes := 'ID_INTERACOES=' || :OLD.ID_INTERACOES || ', ID_USUARIO=' || :OLD.ID_USUARIO || 
-                             ', ID_EVENTO=' || :OLD.ID_EVENTO || ', ID_TP_INTERACO=' || :OLD.ID_TP_INTERACO || 
-                             ', ID_INCIDENTES=' || :OLD.ID_INCIDENTES || ', INTERACOES=' || :OLD.INTERACOES;
-            v_dados_depois := 'ID_INTERACOES=' || :NEW.ID_INTERACOES || ', ID_USUARIO=' || :NEW.ID_USUARIO || 
-                              ', ID_EVENTO=' || :NEW.ID_EVENTO || ', ID_TP_INTERACOES=' || :NEW.ID_TP_INTERACO || 
-                              ', ID_INCIDENTES=' || :NEW.ID_INCIDENTES || ', INTERACOES=' || :NEW.INTERACOES;
-        ELSIF DELETING THEN
-            v_operacao := 'DELETE';
-            v_dados_antes := 'ID_INTERACOES=' || :OLD.ID_INTERACOES || ', ID_USUARIO=' || :OLD.ID_USUARIO || 
-                             ', ID_EVENTO=' || :OLD.ID_EVENTO || ', ID_TP_INTERACO=' || :OLD.ID_TP_INTERACO || 
-                             ', ID_INCIDENTES=' || :OLD.ID_INCIDENTES || ', INTERACOES=' || :OLD.INTERACOES;
-        END IF;
-     
-        INSERT INTO TB_AUDITORIA (ID_AUDITORIA, TABELA, OPERACAO, USUARIO, DATA_HORA, DADOS_ANTES, DADOS_DEPOIS)
-        VALUES (SEQ_AUDITORIA.NEXTVAL, 'TB_INTERACOES', v_operacao, USER, SYSTIMESTAMP, v_dados_antes, v_dados_depois);
+        NULL; 
     END AUDIT_TB_INTERACOES;
-
 END AUDIT_PKG;
+
